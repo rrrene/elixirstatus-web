d4430b1e03dc4fecfb36fb943db76f14da4d25d4
René Föhring <rf@bamaru.de>
2015-07-09 20:32:25 +0200

    Update fake announcements

--- priv/repo/seeds/postings_dev.exs
+++ priv/repo/seeds/postings_dev.exs
@@ -2,26 +2,108 @@ alias ElixirStatus.Posting
 alias ElixirStatus.Repo
 alias ElixirStatus.Publisher
 
-uid = ElixirStatus.UID.generate(Posting)
-%Posting{
-  user_id: 1,
-  uid: uid,
-  permalink: Publisher.permalink(uid, "test post"),
-  text: "bla bla bal",
-  title: "test post",
-  scheduled_at: Ecto.DateTime.utc,
-  published_at: Ecto.DateTime.utc,
-  public: true
-} |> Repo.insert!
+title = "elixirstatus: Update on screendesign"
+text = """
+So, this is what the current state of affairs looks like.
 
+You can follow the development of this on Github: [rrrene/elixirstatus-web](https://github.com/rrrene/elixirstatus-web)
+
+Goto [elixirstatus.com](http://elixirstatus.com) to pre-register your GitHub account for posting.
+"""
 uid = ElixirStatus.UID.generate(Posting)
-%Posting{
-  user_id: 2,
-  uid: uid,
-  permalink: Publisher.permalink(uid, "test post 2"),
-  text: "bla bla bal 2",
-  title: "test post 2",
-  scheduled_at: Ecto.DateTime.utc,
-  published_at: Ecto.DateTime.utc,
-  public: true
-} |> Repo.insert!
+%Posting{user_id: 1, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "The new, shiny Elixir Jobs"
+text = """
+Elixir Jobs is the best place to find, list jobs and developer community space specifically for Elixir Programming Language.
+
+Check out our new design: http://jobs.elixirdose.com/
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 2, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "Learning Elixir is one year old today!"
+text = """
+It’s been one year since my first post on Learning Elixir.
+
+I thought I would take this opportunity to look back over the last year and reflect and to look forward to the next year: http://learningelixir.joekain.com/learning-elixir-year-one-review/
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 3, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "Mongo.Ecto v0.1.0 released!"
+text = """
+Mongo.Ecto is a MongoDB adapter for Ecto.
+
+For detailed information for the Mongo.Ecto module check out the Github repo: https://github.com/michalmuskala/mongodb_ecto
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 4, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "I just wrote a blog post"
+text = """
+This is a fake announcement by ^ this person.
+
+***If you are him or her, you can help this project out:***
+
+[Edit this on GitHub](https://github.com/rrrene/elixirstatus-web/edit/master/priv/repo/seeds/postings_dev.exs) to add a more convincing text!
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 5, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "Version 0.0.0 released"
+text = """
+This is a fake announcement by ^ this person.
+
+***If you are him or her, you can help this project out:***
+
+[Edit this on GitHub](https://github.com/rrrene/elixirstatus-web/edit/master/priv/repo/seeds/postings_dev.exs) to add a more convincing text!
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 6, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "New project: FluxCapacitorEx"
+text = """
+This is a fake announcement by ^ this person.
+
+***If you are him or her, you can help this project out:***
+
+[Edit this on GitHub](https://github.com/rrrene/elixirstatus-web/edit/master/priv/repo/seeds/postings_dev.exs) to add a more convincing text!
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 7, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
+
+title = "I got lazy writing fake announcements ..."
+text = """
+This is a fake announcement by ^ this person.
+
+***If you are him or her, you can help this project out:***
+
+[Edit this on GitHub](https://github.com/rrrene/elixirstatus-web/edit/master/priv/repo/seeds/postings_dev.exs) to add a more convincing text!
+"""
+uid = ElixirStatus.UID.generate(Posting)
+%Posting{user_id: 8, uid: uid, permalink: Publisher.permalink(uid, title), text: text, title: title, scheduled_at: nil, published_at: Ecto.DateTime.utc, public: true}
+  |> Repo.insert!
+
+# ---------------------------------------------------------------------------
--- priv/repo/seeds/users_dev.exs
+++ priv/repo/seeds/users_dev.exs
@@ -1,16 +1,69 @@
 alias ElixirStatus.User
 alias ElixirStatus.Repo
 
+all = [
   %User{
-    full_name: "test user",
-    user_name: "test",
-    email: "test@test.com",
+    id: 1,
+    full_name: "René Föhring",
+    user_name: "rrrene",
+    email: "rrrene@example.com",
     provider: "github"
-  } |> Repo.insert!
-
+  },
   %User{
-    full_name: "test user 2",
-    user_name: "test 2",
-    email: "test2@test.com",
+    id: 2,
+    full_name: "",
+    user_name: "rizafahmi",
+    email: "rizafahmi@example.com",
     provider: "github"
-  } |> Repo.insert!
+  },
+  %User{
+    id: 3,
+    full_name: "",
+    user_name: "joekain",
+    email: "joekain@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 4,
+    full_name: "",
+    user_name: "michalmuskala",
+    email: "michalmuskala@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 5,
+    full_name: "Hans",
+    user_name: "Hanspagh",
+    email: "Hanspagh@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 6,
+    full_name: "",
+    user_name: "h4cc",
+    email: "h4cc@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 7,
+    full_name: "",
+    user_name: "philnash",
+    email: "philnash@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 8,
+    full_name: "",
+    user_name: "nithinbekal",
+    email: "nithinbekal@example.com",
+    provider: "github"
+  },
+  %User{
+    id: 100,
+    full_name: "",
+    user_name: "your-name-here",
+    email: "your-name-here@example.com",
+    provider: "github"
+  }
+]
+Enum.each(all, &Repo.insert!(&1))
