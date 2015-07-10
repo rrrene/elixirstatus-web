834f3ed48f00a031bae9faf68f9cc60f0afe9d9b
René Föhring <rf@bamaru.de>
2015-07-10 21:33:57 +0200

    Update Auth

--- lib/elixir_status/auth.ex
+++ lib/elixir_status/auth.ex
@@ -1,20 +1,27 @@
 defmodule ElixirStatus.Auth do
+  @moduledoc """
+  The Auth module holds functions which handle authorization.
+  """
+
+  @doc "Returns true if the logged in user is an admin."
   def admin?(conn) do
-    false
+    case current_user(conn) do
+      nil   -> false
+      _user -> false # TODO: implement list of admin users
+    end
   end
 
-  def logged_in?(conn) do
-    !is_nil current_user(conn)
-  end
-
-  def current_user(conn) do
-    conn.assigns[:current_user]
-  end
-
+  @doc "Returns true if the given +object+ belongs to the logged in user."
   def belongs_to_current_user?(conn, object) do
     case current_user(conn) do
       nil  -> false
       user -> object.user_id == user.id
     end
   end
+
+  @doc "Returns the current user."
+  def current_user(conn), do: conn.assigns[:current_user]
+
+  @doc "Returns true if a user is logged in."
+  def logged_in?(conn), do: !is_nil current_user(conn)
 end
