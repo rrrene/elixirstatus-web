f24647e8e2b88584f8c8672a74f8094378f63fe0
René Föhring <rf@bamaru.de>
2015-07-13 00:55:19 +0200

    Update human_readable_date

--- web/views/view_helper.ex
+++ web/views/view_helper.ex
@@ -1,11 +1,31 @@
 defmodule ViewHelper do
+  use Timex
+
   # why does `import ElixirStatus.Auth` not work here?
   def current_user(conn), do: ElixirStatus.Auth.current_user(conn)
   def logged_in?(conn), do: ElixirStatus.Auth.logged_in?(conn)
   def admin?(conn), do: ElixirStatus.Auth.admin?(conn)
 
   def human_readable_date(date) do
-    date
+    {:ok, date} = Ecto.DateTime.dump(date)
+    date = Date.from(date)
+    {:ok, readable} = if this_year?(date) do
+                        if today?(date) do
+                          {:ok, "Today"}
+                        else
+                          DateFormat.format(date, "%e %b", :strftime)
+                        end
+                      else
+                        DateFormat.format(date, "%e %b %Y", :strftime)
+                      end
+    readable
+  end
+
+  defp this_year?(date), do: date.year == Ecto.DateTime.utc.year
+
+  defp today?(date) do
+    now = Ecto.DateTime.utc
+    date.day == now.day && date.month == now.month && date.year == now.year
   end
 
   def sanitize(text) do
