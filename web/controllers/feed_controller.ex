15720d8d1256366299db082f2bbeb5407c2ccc92
Hans <hanspagh@gmail.com>
2015-07-14 22:47:59 +0200

    add rss

---
+++ web/controllers/feed_controller.ex
@@ -0,0 +1,10 @@
+defmodule ElixirStatus.FeedController do
+  use ElixirStatus.Web, :controller
+
+  alias ElixirStatus.PostingController
+
+  def rss(conn, _params) do
+    postings = PostingController.get_all
+    render(conn, "index.xml",postings: postings.entries)
+  end
+end
