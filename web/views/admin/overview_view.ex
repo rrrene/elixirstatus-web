d55fe79597c2e39703b5e1ddb88966d5bbce60d5
René Föhring <rf@bamaru.de>
2015-07-24 00:50:41 +0200

    Add short_links to admin

--- web/views/admin/overview_view.ex
+++ web/views/admin/overview_view.ex
@@ -1,3 +1,10 @@
 defmodule ElixirStatus.Admin.OverviewView do
   use ElixirStatus.Web, :view
+
+
+  def to_short_uid(permalink) do
+    uid = "/p/#{permalink}"
+            |> ElixirStatus.URL.from_path
+            |> ElixirStatus.LinkShortener.to_uid
+  end
 end
