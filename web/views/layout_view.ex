5322a50e02ac202869826dec4897e57d27d7ce84
René Föhring <rf@bamaru.de>
2015-07-28 18:47:00 +0200

    Fix html title

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -4,6 +4,13 @@ defmodule ElixirStatus.LayoutView do
   alias ElixirStatus.Posting
   alias ElixirStatus.URL
 
+  def html_title(assigns) do
+    case assigns[:posting] do
+      nil -> ElixirStatus.Meta.html_title
+      posting -> posting.title
+    end
+  end
+
   def og_image_url(assigns) do
     og_image_path(assigns) |> URL.from_path
   end
@@ -12,7 +19,7 @@ defmodule ElixirStatus.LayoutView do
     case assigns[:posting] do
       nil -> "/images/logo.png"
       %Posting{user: %Ecto.Association.NotLoaded{}} -> "/images/logo.png"
-      %Posting{user: user} -> "/avatar/github/#{user.user_name}.jpg"
+      %Posting{user: user} -> "/avatar/github/#{user.user_name}"
     end
   end
 end
